2a3a1eb7ffc52f8bec6b949f1ac3de647fccd40f
Armaan <justarmaansidhu@gmail.com>
2025-12-21 18:46:53 -0500

    Finished Chapter - 3 of Introduction to Python from Datacamp

---
+++ 01-Intro-to-Python/03-Functions-and-Packages/packages.py
@@ -0,0 +1,23 @@
+# Import the math package
+import math as ma
+
+# Calculate C
+C = 2 * 0.43 * ma.pi
+
+# Calculate A
+A = ma.pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+# Import pi function of math package
+from math import pi as pi
+
+# Calculate C
+C = 2 * 0.43 * pi
+
+# Calculate A
+A = pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
