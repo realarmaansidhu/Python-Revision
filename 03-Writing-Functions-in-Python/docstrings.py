f39a9acd0294f94faabc4a0b4bb16345ee25bc8f
Armaan <justarmaansidhu@gmail.com>
2025-12-26 21:30:39 -0500

    Finished Chapter - 1 of Writing Functions in Python from Datacamp

---
+++ 03-Writing-Functions-in-Python/docstrings.py
@@ -0,0 +1,88 @@
+def count_letter(content, letter):
+  """Count the number of times `letter` appears in `content`.
+
+  Args:
+    content (str): The string to search.
+    letter (str): The letter to search for.
+
+  Returns:
+    int
+
+  # Add a section detailing what errors might be raised
+  Raises:
+    ValueError: If `letter` is not a one-character string.
+  """
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('`letter` must be a single character string.')
+  return len([char for char in content if char == letter])
+
+import inspect
+
+def build_tooltip(function):
+  """Create a tooltip for any function that shows the
+  function's docstring.
+
+  Args:
+    function (callable): The function we want a tooltip for.
+
+  Returns:
+    str
+  """
+  # Get the docstring for the "function" argument by using inspect
+  docstring = inspect.getdoc(function)
+  border = '#' * 28
+  return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
+
+def standardize(column):
+  """Standardize the values in a column.
+
+  Args:
+    column (pandas Series): The data to standardize.
+
+  Returns:
+    pandas Series: the values as z-scores
+  """
+  # Finish the function so that it returns the z-scores
+  z_score = (column - column.mean()) / column.std()
+  return z_score
+
+# # Use the standardize() function to calculate the z-scores
+# df['y1_z'] = standardize(df['y1_gpa'])
+# df['y2_z'] = standardize(df['y2_gpa'])
+# df['y3_z'] = standardize(df['y3_gpa'])
+# df['y4_z'] = standardize(df['y4_gpa'])
+
+def mean(values):
+  """Get the mean of a sorted list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the mean() function
+  mean = sum(values)
+  return mean / len(values)
+
+def median(values):
+  """Get the median of a sorted list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the median() function
+  values = sorted(values)
+  midpoint = int(len(values) / 2)
+  if len(values) % 2 == 0:
+    median = (values[midpoint - 1] + values[midpoint]) / 2
+  else:
+    median = values[midpoint]
+  return median
