7da156d2aba5d86b4a4348c3d864e6a83a4384b5
Armaan <justarmaansidhu@gmail.com>
2026-01-29 14:49:41 -0500

    Practiced Scope in Python

---
+++ 03-Writing-Functions-in-Python/scope.py
@@ -0,0 +1,57 @@
+# Add a keyword that lets us update call_count from inside the function.
+
+call_count = 0
+
+def my_function():
+  # Use a keyword that lets us update call_count 
+  global call_count
+  call_count += 1
+  
+  print("You've called my_function() {} times!".format(
+    call_count
+  ))
+  
+for _ in range(20):
+  my_function()
+
+# Add a keyword that lets us modify file_contents from inside save_contents().
+
+def read_files():
+  file_contents = None
+  
+  def save_contents(filename):
+    # Add a keyword that lets us modify file_contents
+    nonlocal file_contents
+    if file_contents is None:
+      file_contents = []
+    with open(filename) as fin:
+      file_contents.append(fin.read())
+      
+  for filename in ['1984.txt', 'MobyDick.txt', 'CatsEye.txt']:
+    save_contents(filename)
+    
+  return file_contents
+
+print('\n'.join(read_files()))
+
+# Add a keyword to done in check_is_done() so that wait_until_done() eventually stops looping.
+
+from random import random
+
+def wait_until_done():
+  def check_is_done():
+    # Add a keyword so that wait_until_done() 
+    # doesn't run forever
+    global done
+    if random.random() < 0.1:
+      done = True
+      
+  while not done:
+    check_is_done()
+
+done = False
+wait_until_done()
+
+print('Work done? {}'.format(done))
+
+# By adding global done in check_is_done(), you ensure that the done being referenced is the one that was set to False before wait_until_done() was called. Withou2\t this keyword, wait_until_done() would loop forever because the done = True in check_is_done() would only be changing a variable that is local to check_is_done(). Understanding what scope your variables are in will help you debug tricky situations like this one.
