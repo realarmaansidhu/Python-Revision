7a37f4acb0a1994761e9e094759ff19276fca93b
Armaan <justarmaansidhu@gmail.com>
2026-02-07 23:29:18 -0500

    Updated the Employee Class to include a Constructor

--- 04-Object-Oriented-Programming-in-Python/01-OOP-Fundamentals/classes.py
+++ 04-Object-Oriented-Programming-in-Python/01-OOP-Fundamentals/classes.py
@@ -24,3 +24,27 @@ mon_sal = emp.monthly_salary()
 
 # Print mon_sal
 print(mon_sal)
+
+# Python Code to Implement Constructors on an Employee Class
+
+# Import datetime from datetime
+from datetime import datetime
+
+class Employee:
+    
+    def __init__(self, name, salary=0):
+        self.name = name
+        if salary > 0:
+          self.salary = salary
+        else:
+          self.salary = 0
+          print("Invalid salary!")
+          
+        # Add the hire_date attribute and set it to today's date
+        self.hire_date = datetime.today()
+        
+   # ...Other methods omitted for brevity ...
+      
+emp = Employee("Korel Rossi", -1000)
+print(emp.name)
+print(emp.salary)
