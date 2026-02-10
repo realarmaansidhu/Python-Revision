1078c45a0fc18917b75765afd917b046740c0c74
Armaan <justarmaansidhu@gmail.com>
2026-02-09 20:12:50 -0500

    Updated the code to practice updating Class Attributes

--- 04-Object-Oriented-Programming-in-Python/01-OOP-Fundamentals/class-attributes.py
+++ 04-Object-Oriented-Programming-in-Python/01-OOP-Fundamentals/class-attributes.py
@@ -2,6 +2,7 @@
 
 class Player:
     MAX_POSITION = 10
+    MAX_SPEED = 3
     
     def __init__(self):
         self.position = 0
@@ -23,3 +24,26 @@ p = Player(); p.draw()
 p.move(4); p.draw()
 p.move(5); p.draw()
 p.move(3); p.draw()
+
+# Create Players p1 and p2
+p1, p2 = Player(), Player()
+
+print("MAX_SPEED of p1 and p2 before assignment:")
+# Print p1.MAX_SPEED and p2.MAX_SPEED
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+# ---MODIFY THIS LINE--- 
+p1.MAX_SPEED = 5
+# Print P1's Max Speed after individual assignment
+print(p1.MAX_SPEED)
+Player.MAX_SPEED = 7
+
+print("MAX_SPEED of p1 and p2 after assignment:")
+# Print p1.MAX_SPEED and p2.MAX_SPEED
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+print("MAX_SPEED of Player:")
+# Print Player.MAX_SPEED
+print(Player.MAX_SPEED)
