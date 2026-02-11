b0b9df2a0ee5cb2613ab13941cd67a7a6b65f0e8
Armaan <justarmaansidhu@gmail.com>
2026-02-10 19:16:36 -0500

    Practiced Method Inheritence through Employee and Maanger classes, noting Python's no fallback approach to inheritence as compared to Java

--- 04-Object-Oriented-Programming-in-Python/02-Inheritence-and-Polymorphism/class-attributes.py
+++ 04-Object-Oriented-Programming-in-Python/02-Inheritence-and-Polymorphism/class-attributes.py
@@ -47,3 +47,19 @@ print(p2.MAX_SPEED)
 print("MAX_SPEED of Player:")
 # Print Player.MAX_SPEED
 print(Player.MAX_SPEED)
+
+# Updated Python Code to implement a Racer Class inheriting Class Attributes from the Player Class
+
+# Create a Racer class and set MAX_SPEED to 5
+class Racer(Player):
+    MAX_SPEED = 5
+ 
+# Create a Player and a Racer objects
+p = Player()
+r = Racer()
+
+print("p.MAX_SPEED = ", p.MAX_SPEED)
+print("r.MAX_SPEED = ", r.MAX_SPEED)
+
+print("p.MAX_POSITION = ", p.MAX_POSITION)
+print("r.MAX_POSITION = ", r.MAX_POSITION)
